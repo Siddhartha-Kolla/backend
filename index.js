8d1994cc5fa98680aa6a57952b11e07ae8a50583
Siddhartha-Kolla <87278444+Siddhartha-Kolla@users.noreply.github.com>
2024-07-08 15:27:25 +0200

    Changed it so that it can also work  for homepage

--- index.js
+++ index.js
@@ -29,8 +29,35 @@ function ReadDatabase(category, functionToCallWithRows) {
   db.close();
 }
 
+function ReadDatabaseAll(functionToCallWithRows) {
+  const sqlite3 = require('sqlite3').verbose();
+  // open the database
+  let db = new sqlite3.Database('./service.db');
+  
+  let sql = `SELECT *
+              FROM Service
+              ORDER BY NAME`;
+  
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    functionToCallWithRows(rows)
+  });
+  
+  // close the database connection
+  db.close();
+}
+
 app.get('/categories', (req, res) => {
   ReadDatabase(req.query.category, (rows) => {
+    res.send({data: rows});
+  })
+  // res.json({ message: "Hello from serversss!" });
+})
+
+app.get('/all', (req, res) => {
+  ReadDatabaseAll((rows) => {
     res.send({data: rows});
   })
   // res.json({ message: "Hello from serversss!" });
